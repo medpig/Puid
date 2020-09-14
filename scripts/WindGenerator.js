aed0f9549784ce90aa8ca28f40c22171dd6553e3
medpig <medpig@yandex.ru>
2020-09-14 12:59:02 +0300

    Initial commit

---
+++ scripts/WindGenerator.js
@@ -0,0 +1,13 @@
+var i=2*7
+const windmill = extendContent(Wall, "LugelWindGenerator", {
+    drawLayer: function(tile){
+        Draw.rect(Core.atlas.find(this.name+"-rotator"), tile.drawx(), tile.drawy(), Time.time()*-4);
+    }, 
+   
+    getPowerProduction(tile){
+        return i;
+    }
+});
+windmill.layer = Layer.turret;
+windmill.hasPower = true;
+windmill.outputsPower = true;
